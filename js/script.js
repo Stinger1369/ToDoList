9027f736a10e83aea1ee989f36f0ab60ee617e69
Stinger1369 <bilelzara@gmail.com>
2023-11-13 10:50:53 +0100

    ADD DELETE BUTON

--- js/script.js
+++ js/script.js
@@ -1,32 +1,24 @@
-// Create a "close" button and append it to each list item
-var addTask = document.querySelector('.addTask');
 var taskList = document.querySelector('.taskList');
 var task = document.querySelector('#task');
 var submit = document.querySelector('button[type="submit"]');
-var close = document.createElement('span');
-var closeText = document.createTextNode('X');
-close.appendChild(closeText);
-close.classList.add('close');
-addTask.appendChild(close);
+
 submit.addEventListener('click', function(){
     var taskValue = task.value;
     var taskItem = document.createElement('li');
     var taskItemText = document.createTextNode(taskValue);
     taskItem.appendChild(taskItemText);
+
+    var deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    deleteButton.className = 'delete';
+    taskItem.appendChild(deleteButton);
+
     taskList.appendChild(taskItem);
+
     task.value = '';
     task.focus();
-    taskItem.appendChild(close);
-    close.addEventListener('click', function(){
+
+    deleteButton.addEventListener('click', function() {
         taskList.removeChild(taskItem);
-    })
-})
-// delete taskItem
-var taskItem = document.querySelectorAll('li');
-for(var i = 0; i < taskItem.length; i++){
-    taskItem[i].addEventListener('click', function(){
-        this.classList.toggle('checked');
-    })
-}
-
-
+    });
+});
