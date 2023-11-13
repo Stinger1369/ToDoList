ea124a1548475e0199cde5f0c0987374ed248ba7
Stinger1369 <bilelzara@gmail.com>
2023-11-13 11:19:11 +0100

    add edit

--- js/script.js
+++ js/script.js
@@ -2,29 +2,65 @@ var taskList = document.querySelector('.taskList');
 var task = document.querySelector('#task');
 var submit = document.querySelector('button[type="submit"]');
 
-submit.addEventListener('click', function(){
+submit.addEventListener('click', function() {
     var taskValue = task.value;
     var taskItem = document.createElement('li');
-    var taskItemText = document.createTextNode(taskValue);
-    taskItem.appendChild(taskItemText);
-    console.log(taskItem);
+
+    var taskInput = document.createElement('input');
+    taskInput.type = 'text';
+    taskInput.value = taskValue;
+    taskInput.readOnly = true;
+    taskInput.className = 'task-input';
+    taskItem.appendChild(taskInput);
+
     var deleteButton = document.createElement('button');
     deleteButton.textContent = 'Delete';
     deleteButton.className = 'delete';
     taskItem.appendChild(deleteButton);
 
+    var editButton = document.createElement('button');
+    editButton.textContent = 'Edit';
+    editButton.className = 'edit';
+    taskItem.appendChild(editButton);
+
+    var dateElement = document.createElement('span');
+    dateElement.textContent = getCurrentDateString();
+    dateElement.className = 'task-date';
+    taskItem.appendChild(dateElement);
+
     taskList.appendChild(taskItem);
-     var currentDate = new Date();
-     var dateString = currentDate.toLocaleDateString() + ' ' + currentDate.toLocaleTimeString();
-     var dateElement = document.createElement('span');
-     dateElement.textContent = dateString;
-     dateElement.className = 'task-date';
-     taskItem.appendChild(dateElement);
     task.value = '';
     task.focus();
 
     deleteButton.addEventListener('click', function() {
         taskList.removeChild(taskItem);
     });
+
+    editButton.addEventListener('click', function() {
+        if (taskInput.readOnly) {
+            taskInput.readOnly = false;
+            taskInput.focus();
+        } else {
+            taskInput.readOnly = true;
+            dateElement.textContent = getCurrentDateString(); 
+        }
+    });
+
+    taskInput.addEventListener('blur', function() {
+        taskInput.readOnly = true;
+        dateElement.textContent = getCurrentDateString(); 
+    });
+
+    taskInput.addEventListener('keypress', function(e) {
+        if (e.key === 'Enter') {
+            taskInput.readOnly = true;
+            taskInput.blur(); 
+            dateElement.textContent = getCurrentDateString();
+        }
+    });
 });
 
+function getCurrentDateString() {
+    var currentDate = new Date();
+    return currentDate.toLocaleDateString() + ' ' + currentDate.toLocaleTimeString();
+}
