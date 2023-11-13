f808f63811e91cf24259d450891e410e1d554223
Stinger1369 <bilelzara@gmail.com>
2023-11-13 10:26:14 +0100

    second commit

--- js/script.js
+++ js/script.js
@@ -1,10 +1,32 @@
 // Create a "close" button and append it to each list item
+var addTask = document.querySelector('.addTask');
+var taskList = document.querySelector('.taskList');
+var task = document.querySelector('#task');
+var submit = document.querySelector('button[type="submit"]');
+var close = document.createElement('span');
+var closeText = document.createTextNode('X');
+close.appendChild(closeText);
+close.classList.add('close');
+addTask.appendChild(close);
+submit.addEventListener('click', function(){
+    var taskValue = task.value;
+    var taskItem = document.createElement('li');
+    var taskItemText = document.createTextNode(taskValue);
+    taskItem.appendChild(taskItemText);
+    taskList.appendChild(taskItem);
+    task.value = '';
+    task.focus();
+    taskItem.appendChild(close);
+    close.addEventListener('click', function(){
+        taskList.removeChild(taskItem);
+    })
+})
+// delete taskItem
+var taskItem = document.querySelectorAll('li');
+for(var i = 0; i < taskItem.length; i++){
+    taskItem[i].addEventListener('click', function(){
+        this.classList.toggle('checked');
+    })
+}
 
 
-// Click on a close button to hide the current list item
-
-
-// Add a "checked" symbol when clicking on a list item
-
-
-// Create a new list item when clicking on the "Add" button
