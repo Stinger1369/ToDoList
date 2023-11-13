15cb43359abc77c1d3388d0eb8927114e0c65249
Stinger1369 <bilelzara@gmail.com>
2023-11-13 10:25:26 +0100

    firest commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Create a "close" button and append it to each list item
+
+
+// Click on a close button to hide the current list item
+
+
+// Add a "checked" symbol when clicking on a list item
+
+
+// Create a new list item when clicking on the "Add" button
